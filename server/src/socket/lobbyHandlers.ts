744afcd900904b424db1ea026d3757cc236df9e2
EnzoGrn <enzogrn13@gmail.com>
2026-06-09 10:55:04 +0200

    feat(lobby): add lobby:create socket event

--- server/src/socket/lobbyHandlers.ts
+++ server/src/socket/lobbyHandlers.ts
@@ -1,5 +1,20 @@
 import { Server, Socket } from 'socket.io';
+import { createLobby } from '../services/lobbyService';
+import { redis } from '../services/redis';
 
 export function registerLobbyHandlers(io: Server, socket: Socket) {
-  // TODO
+
+  socket.on('lobby:create', async ({ playerName }: { playerName: string }, callback) => {
+    try {
+      const lobby = await createLobby(socket.id, playerName);
+
+      await redis.set(`player:${socket.id}`, lobby.code);
+
+      socket.join(lobby.code);
+      callback({ ok: true, lobby });
+    } catch (err) {
+      callback({ ok: false, error: (err as Error).message });
+    }
+  });
+
 }
